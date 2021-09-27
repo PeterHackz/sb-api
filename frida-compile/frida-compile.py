12d09d40bb62dfdc0abe01dd5793eaff247a3148
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2021-09-27 22:11:06 +0300

    fixed sample.js name

--- frida-compile/frida-compile.py
+++ frida-compile/frida-compile.py
@@ -1,1 +1,1 @@
-open("compiled.txt", "wb").write(__import__("requests").post("https://sb-apis.repl.co/sb9838/tools/frida-compile", open("sample.txt", "r").read()).content)
+open("compiled.txt", "wb").write(__import__("requests").post("https://sb-apis.repl.co/sb9838/tools/frida-compile", open("sample.js", "r").read()).content)
